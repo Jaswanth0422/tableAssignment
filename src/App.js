b36492203392bd566a0e941cb40b7b617b8d41ef
Jaswanth0422 <37337715+Jaswanth0422@users.noreply.github.com>
2021-08-07 17:52:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+//import Users from './components/userscomponent';
+//import Table from './components/classTable';
+import TableContainer from './components/tableContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <TableContainer/>
+      {/* <Users/> */}
+      {/* <Table/> */}
+    </div>
+  );
+}
+
+export default App;
